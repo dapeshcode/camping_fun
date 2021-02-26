6957015d4f1a888ea9b7587e7179628a3eb618c9
dapeshcode <ampeccia@gmail.com>
2021-02-25 18:27:05 -0600

    completed all deliverables

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,16 @@
+class SignupsController < ApplicationController
+
+    def new 
+        @signup = Signup.new
+        @campers = Camper.all 
+        @activities = Activity.all 
+    end 
+
+    def create
+        signup_params = params.require(:signup).permit(:camper_id, :activity_id)
+        Signup.create(signup_params)
+        camper = Camper.find(params[:signup][:camper_id])
+        redirect_to camper_path(camper)
+
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/campers', to: "campers#index", as: "campers"
-  get '/activities', to: "activities#index", as: "activities"
+  resources :signups
+  resources :campers 
+  resources :activities
+  # get '/campers', to: "campers#index", as: "campers"
+  # get '/activities', to: "activities#index", as: "activities"
+  # get '/activities/:id', to: "activities#show", as: 'activity'
+  # get '/signups', to: "signups#index", as: "signups"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
